2bea3a713450fbbfbca503e2d3b7e440e5e4fdfb
Advitiya Yadav <advitiyayadav2105@gmail.com>
2025-12-30 04:55:17 +0530

    Streamlit front end code version 1

---
+++ streamlit_app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import requests
+
+st.title("🪙 Crypto Price Tracker")
+
+coin = st.selectbox("Enter coin name (Bitcoin, Ethereum, Solana)", ["bitcoin", "ethereum", "solana"])
+currency = st.selectbox("Select currency", ["usd", "inr", "eur"])
+
+if st.button("Get Price"):
+    if coin == "":
+        st.error("Please select a valid coin")
+    else:
+        url = "http://127.0.0.1:5000/price"
+        params = {
+            "coin": coin,
+            "currency": currency
+        }
+
+        r = requests.get(url, params=params)
+        data = r.json()
+
+        if "error" in data:
+            st.error(data["error"])
+        else:
+            st.metric(
+                label=f"{coin.upper()} price",
+                value=f"{data['price']} {currency.upper()}",
+                delta=f"{data['change_24h']}%"
+            )
